06739a2690b08e90260007baa17370bc676e2ff6
fr640504-dotcom <fr640504@gmail.com>
2026-06-08 10:08:27 -0600

    Translate settings views to Spanish

--- resources/views/livewire/settings/delete-user-form.blade.php
+++ resources/views/livewire/settings/delete-user-form.blade.php
@@ -24,34 +24,34 @@ new class extends Component {
 
 <section class="mt-10 space-y-6">
     <div class="relative mb-5">
-        <flux:heading>{{ __('Delete Account') }}</flux:heading>
-        <flux:subheading>{{ __('Delete your account and all of its resources') }}</flux:subheading>
+        <flux:heading>{{ __('Eliminar cuenta') }}</flux:heading>
+        <flux:subheading>{{ __('Elimina tu cuenta y todos sus recursos') }}</flux:subheading>
     </div>
 
     <flux:modal.trigger name="confirm-user-deletion">
         <flux:button variant="danger" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
-            {{ __('Delete Account') }}
+            {{ __('Eliminar cuenta') }}
         </flux:button>
     </flux:modal.trigger>
 
     <flux:modal name="confirm-user-deletion" :show="$errors->isNotEmpty()" focusable class="max-w-lg">
         <form wire:submit="deleteUser" class="space-y-6">
             <div>
-                <flux:heading size="lg">{{ __('Are you sure you want to delete your account?') }}</flux:heading>
+                <flux:heading size="lg">{{ __('¿Estás seguro de que quieres eliminar tu cuenta?') }}</flux:heading>
 
                 <flux:subheading>
-                    {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                    {{ __('Una vez que se elimine tu cuenta, todos sus recursos y datos se eliminarán permanentemente. Ingresa tu contraseña para confirmar que deseas eliminar tu cuenta de forma permanente.') }}
                 </flux:subheading>
             </div>
 
-            <flux:input wire:model="password" id="password" label="{{ __('Password') }}" type="password" name="password" />
+            <flux:input wire:model="password" id="password" label="{{ __('Contraseña') }}" type="password" name="password" />
 
             <div class="flex justify-end space-x-2">
                 <flux:modal.close>
-                    <flux:button variant="filled">{{ __('Cancel') }}</flux:button>
+                    <flux:button variant="filled">{{ __('Cancelar') }}</flux:button>
                 </flux:modal.close>
 
-                <flux:button variant="danger" type="submit">{{ __('Delete Account') }}</flux:button>
+                <flux:button variant="danger" type="submit">{{ __('Eliminar cuenta') }}</flux:button>
             </div>
         </form>
     </flux:modal>
